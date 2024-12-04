264c8da330e81c269fb883f7e137396e17fbc15a
ThiagoCoderfull <thiago.dev.x@gmail.com>
2024-12-04 18:19:26 -0300

    Add mongoose dependency and connect to MongoDB

--- 2docker-node/index.js
+++ 2docker-node/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const mongoose = require("mongoose")
 const app = express()
 const port = 3000
+
+mongoose.connect("")
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.log("Error connecting to MongoDB", err))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
