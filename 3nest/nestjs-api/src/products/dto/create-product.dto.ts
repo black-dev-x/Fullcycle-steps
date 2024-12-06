3eff355d2548b0deaef8e2f20a326f253503d134
ThiagoCoderfull <thiago.dev.x@gmail.com>
2024-12-06 13:23:36 -0300

    Add global validation pipe and prepare script

--- 3nest/nestjs-api/src/products/dto/create-product.dto.ts
+++ 3nest/nestjs-api/src/products/dto/create-product.dto.ts
@@ -1,6 +1,23 @@
+import { IsNotEmpty, IsNumber, IsString, Matches, MaxLength, Min, isNotEmpty } from 'class-validator'
+
 export class CreateProductDto {
+  @IsNotEmpty()
+  @MaxLength(255)
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @Matches(/^[a-z0-9-]+$/, { message: 'slug must contain only lowercase letters, numbers and dashes' })
+  @IsString()
   slug: string;
+
+  @IsNotEmpty()
+  @MaxLength(500)
+  @IsString()
   description: string;
+
+  @Min(1)
+  @IsNumber({maxDecimalPlaces: 2})
+  @IsNotEmpty()
   price: number;
 }
