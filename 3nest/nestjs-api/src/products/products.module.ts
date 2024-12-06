89d68ca5a1acb12f3c29355a33c92044202bcc19
ThiagoCoderfull <thiago.dev.x@gmail.com>
2024-12-06 12:46:19 -0300

    Update product name and slug in API request

--- 3nest/nestjs-api/src/products/products.module.ts
+++ 3nest/nestjs-api/src/products/products.module.ts
@@ -1,9 +1,29 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { ProductsController } from './products.controller';
+import { PrismaService } from 'src/prisma/prisma.service'
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
 })
-export class ProductsModule {}
+export class ProductsModule implements OnModuleInit {
+
+  constructor(private productService: ProductsService, private prismaService: PrismaService) {}
+
+  private async initializeMockData() {
+    await this.prismaService.product.deleteMany();
+    new Array(10).fill(0).forEach( async (_, index) => {
+      await this.productService.create({
+        name: `Product ${index}`,
+        description: `Description ${index}`,
+        slug: `product-${index}`,
+        price: Math.random() * 1000,
+      });
+    })
+  }
+  async onModuleInit() {
+    this.initializeMockData()
+  }
+
+}
