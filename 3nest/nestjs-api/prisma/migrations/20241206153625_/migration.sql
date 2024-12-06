7c8d2b5e9e483ed7fdf3c9dabfcd346b22d649ce
ThiagoCoderfull <thiago.dev.x@gmail.com>
2024-12-06 12:37:17 -0300

    Add database.sqlite to .gitignore and remove migration.sql files

--- 3nest/nestjs-api/prisma/migrations/20241206153625_/migration.sql
+++ 3nest/nestjs-api/prisma/migrations/20241206153625_/migration.sql
@@ -8,3 +8,6 @@ CREATE TABLE "Product" (
     "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" DATETIME NOT NULL
 );
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_slug_key" ON "Product"("slug");
